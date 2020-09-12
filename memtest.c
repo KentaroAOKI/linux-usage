f574248da6f9f79a05880a760ce2a12b39ba0111
Kentaro Aoki <kentaro.aoki@gmail.com>
2020-09-12 13:57:31 +0000

    add memtest

---
+++ memtest.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#define MEM_COUNT 1024*10
+#define MEM_ALLC 1024*1024
+
+int main(void) {
+    int i;
+    char *allocs[MEM_COUNT];
+    int alloc_count;
+
+    for (i = 0; i < MEM_COUNT; i++)
+    {
+        allocs[i] = malloc(MEM_ALLC);
+        memset(allocs[i], 0, MEM_ALLC);
+        if (allocs[i] == NULL)
+        {
+            printf("Error: %s\n", strerror(errno));
+            break;
+        }
+        if ((i % 100) == 0) {
+            printf("%dGB memory allocated.\n", (i)/1024);
+            sleep(1);
+        }
+    }
+    alloc_count = i;
+    for (i = 0; i < alloc_count; i++)
+    {
+        free(allocs[i]);
+    }
+    printf("Result: %dkB memory allocated.\n", alloc_count * 1000);
+    return 0;
+}
--- os_usage.sh
+++ os_usage.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-echo date,fd,fd_lim,proc,proc_lim,thread,thread_lim
+echo date,fd,fd_lim,proc,proc_lim,thread,thread_lim,mem_available
 while [ true ]
 do
     fd_lim_per_process=`ulimit -n`
     fd_lim=`cat /proc/sys/fs/file-max`
     proc_lim=`cat /proc/sys/kernel/pid_max`
     thread_lim=`cat /proc/sys/kernel/threads-max`
+    mem_available=`cat /proc/meminfo|grep ^MemAvailable|sed -e "s/MemAvailable:[[:space:]]*//g"|sed -e "s/[[:space:]]*kB//g"`
     fd_count=0
     proc_count=0
     thread_count=`ps -mH | wc -l`
@@ -16,6 +17,6 @@ do
         proc_count=`expr 1 + $proc_count`
         proc_name=`cat $procid/status|grep ^Name|sed -e "s/Name:\t//g"`
     done
-    echo `date "+%Y/%m/%d %H:%M:%S"`,$fd_count,$fd_lim,$proc_count,$proc_lim,$thread_count,$thread_lim
+    echo `date "+%Y/%m/%d %H:%M:%S"`,$fd_count,$fd_lim,$proc_count,$proc_lim,$thread_count,$thread_lim,$mem_available
     sleep 1
 done
